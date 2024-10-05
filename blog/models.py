463c296929aed0bdeea9a32834f5e48282253c46
slucaci <serjlucaci@gmail.com>
2024-10-05 20:27:38 +0100

    POSTing and writing to the database

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.CharField(max_length=255, blank=True)
+    excerpt = models.CharField(max_length=500, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
   
 
