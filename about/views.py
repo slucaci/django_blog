0abda54b3c3a0f8525af0a2f177a15e9366f5b75
slucaci <serjlucaci@gmail.com>
2024-10-06 10:27:22 +0100

    dded comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from .models import About
-
+from .forms import CollaborateForm
+from django.contrib import messages
 
 def about_me(request):
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    
+    # Check if the request method is POST
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+    else:
+        # Initialize the form in case of GET request
+        collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, "collaborate_form": collaborate_form},
     )
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),  # Home page
     path('<slug:slug>/', views.post_detail, name='post_detail'),  # Post detail
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+     path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
